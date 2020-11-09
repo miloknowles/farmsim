a84b111af0d7caf5bb5e1b6bdbcdaaf152e1a139
Milo Knowles <mknowles@mit.edu>
2020-11-09 17:54:26 -0500

    Add Nature Starter Kit 2 and first attempt at Giant Kelp

--- unity/Assets/Scripts/WaterEffect.cs
+++ unity/Assets/Scripts/WaterEffect.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 
 
+/**
+ * Projects a sequence of "caustic" images that make the planar ocean floor look like the bottom
+ * of a pool. See: https://en.wikipedia.org/wiki/Caustic_(optics)
+ */
 public class WaterEffect : MonoBehaviour {
 	public float fps = 30.0f;
 	public Texture2D[] frames;
