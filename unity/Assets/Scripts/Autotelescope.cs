295b7326616084729870283c0bb7632c023bee02
Milo Knowles <mknowles@mit.edu>
2020-11-12 09:52:03 -0500

    Example grid that autotelescopes

--- unity/Assets/Scripts/Autotelescope.cs
+++ unity/Assets/Scripts/Autotelescope.cs
@@ -5,24 +5,40 @@ using UnityEngine;
 
 public class Autotelescope : MonoBehaviour
 {
-  // Attach the -y end of the cylinder to this object.
+  public enum LongitudinalAxis { X, Y, Z }
+
+  public GameObject telescope;
+
+  // Attach the - end of the cylinder to this object.
   public GameObject endpoint0;
   public Vector3 offset0;
 
-  // Attach the +y end of the cylinder to this object.
+  // Attach the + end of the cylinder to this object.
   public GameObject endpoint1;
   public Vector3 offset1;
 
+  public LongitudinalAxis longitudinalAxis = LongitudinalAxis.Y;
+
   private float originalScaleX;
   private float originalScaleZ;
+  // private Vector3 originalPosition0;
+  // private Vector3 originalPosition1;
+
+  // By default, assume cylinder is aligned with the +y axis.
+  private Vector3 alignVector = new Vector3(0, 1, 0);
 
   void Start()
   {
-    originalScaleX = this.gameObject.transform.localScale.x;
-    originalScaleZ = this.gameObject.transform.localScale.z;
+    originalScaleX = telescope.transform.localScale.x;
+    originalScaleZ = telescope.transform.localScale.z;
+
+    if (longitudinalAxis == LongitudinalAxis.X) {
+      alignVector = new Vector3(1, 0, 0);
+    } else if (longitudinalAxis == LongitudinalAxis.Z) {
+      alignVector = new Vector3(0, 0, 1);
+    }
   }
 
-  // Update is called once per frame
   void Update()
   {
     // Average of the two endpoints.
@@ -32,12 +48,14 @@ public class Autotelescope : MonoBehaviour
     Vector3 unit_01 = Vector3.Normalize(vector_01);
     float length_01 = vector_01.magnitude;
 
-    Vector3 y_axis = new Vector3(0, 1, 0);
-    Quaternion q_align_y = RotateAlignVectors(y_axis, unit_01);
+    Quaternion q_align_long = RotateAlignVectors(alignVector, unit_01);
 
+    // Position the attached game object at the center of the two endpoints.
     this.gameObject.transform.position = midpoint;
-    this.gameObject.transform.rotation = q_align_y;
-    this.gameObject.transform.localScale = new Vector3(this.originalScaleX, 0.5f*length_01, this.originalScaleZ);
+    this.gameObject.transform.rotation = q_align_long;
+
+    // Make the attached telescope scale to fit between the endpoints.
+    telescope.transform.localScale = new Vector3(this.originalScaleX, 0.5f*length_01, this.originalScaleZ);
   }
 
   // https://stackoverflow.com/questions/1171849/finding-quaternion-representing-the-rotation-from-one-vector-to-another
