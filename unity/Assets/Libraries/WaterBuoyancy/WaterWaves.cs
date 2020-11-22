32a59d5be12d061be8426f9d8b607ce7964d0442
Milo Knowles <mknowles@mit.edu>
2020-11-22 11:13:53 -0500

    Make the farm follow wave height changes

--- unity/Assets/Libraries/WaterBuoyancy/WaterWaves.cs
+++ unity/Assets/Libraries/WaterBuoyancy/WaterWaves.cs
@@ -1,6 +1,9 @@
 ﻿using UnityEngine;
 
-
+/**
+ * Uses Perlin noise to simulate moving surface waves.
+ # From: https://github.com/dbrizov/NaughtyWaterBuoyancy/blob/master/Assets/NaughtyWaterBuoyancy/Scripts/Core/WaterWaves.cs
+ */
 namespace Simulator {
   public class WaterWaves : MonoBehaviour {
     [SerializeField]
@@ -16,14 +19,14 @@ namespace Simulator {
     private float noiseStrength = 0.1f;
 
     private Mesh mesh;
-    private Vector3[] baseVertices;
+    private Vector3[] originalVertices;
     private Vector3[] vertices;
 
     protected virtual void Awake()
     {
       this.mesh = this.GetComponent<MeshFilter>().mesh;
-      this.baseVertices = this.mesh.vertices;
-      this.vertices = new Vector3[this.baseVertices.Length];
+      this.originalVertices = this.mesh.vertices;
+      this.vertices = new Vector3[this.originalVertices.Length];
     }
 
     protected virtual void Start()
@@ -33,15 +36,12 @@ namespace Simulator {
 
     protected virtual void Update()
     {
-      for (var i = 0; i < this.vertices.Length; i++)
-      {
-        var vertex = this.baseVertices[i];
-        vertex.y +=
-          Mathf.Sin(Time.time * this.speed + this.baseVertices[i].x + this.baseVertices[i].y + this.baseVertices[i].z) *
-          (this.height / this.transform.localScale.y);
+      for (int i = 0; i < this.vertices.Length; ++i) {
+        Vector3 vertex = this.originalVertices[i];
 
-        vertex.y += Mathf.PerlinNoise(baseVertices[i].x + this.noiseWalk, baseVertices[i].y /*+ Mathf.Sin(Time.time * 0.1f)*/) * this.noiseStrength;
-
+        // Divide by localScale to make the wave height in global scale.
+        float amplitude = this.height / this.transform.localScale.y;
+        vertex.y += amplitude * CalculateHeightOffset(this.originalVertices[i].x, this.originalVertices[i].z, true);
         this.vertices[i] = vertex;
       }
 
@@ -60,6 +60,23 @@ namespace Simulator {
 
         boxCollider.center = center;
       }
+    }
+
+    /**
+     * Returns the wave height offset (above the flat water level) at a particular XZ location.
+     */
+    public float CalculateHeightOffset(float x, float z, bool noise)
+    {
+      // All vertices parameterized by (x + z = constant) will have the same wave height.
+      // This orients all of the waves diagonally at a 45 deg angle.
+      float offset = Mathf.Sin(Time.time * this.speed + x + z);
+
+      // Apply Perlin noise to make the waves look more realistic than a pure sinewave.
+      if (noise) {
+        offset += Mathf.PerlinNoise(x + this.noiseWalk, 0) * this.noiseStrength;
+      }
+
+      return offset;
     }
   }
 }
---
+++ unity/Assets/Scripts/FollowWaveHeight.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class FollowWaveHeight : MonoBehaviour {
+  public Simulator.WaterWaves waveController;
+
+  // To simulate some damping due to drag, the floating object only respond to some fraction of
+  // wave height changes.
+  public float dampingFactor = 0.2f;
+
+  private Vector3 originalPosition;
+
+  // Start is called before the first frame update
+  void Start()
+  {
+    this.originalPosition = this.transform.position;
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+    if (this.waveController != null) {
+      Vector3 position = this.originalPosition;
+      position.y += this.dampingFactor * this.waveController.CalculateHeightOffset(this.originalPosition.x, this.originalPosition.z, false);
+      this.transform.position = position;
+    }
+  }
+}
