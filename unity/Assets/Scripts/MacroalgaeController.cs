0dae8015a77b4449088d45ec4ed830cbd5f017d7
Milo Knowles <mknowles@mit.edu>
2020-11-24 17:21:12 -0500

    Add macroalgae control and repair material

---
+++ unity/Assets/Scripts/MacroalgaeController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class MacroalgaeController : MonoBehaviour {
+  [Range(0.0f, 1.0f)]
+  public float maturity = 1.0f;       // Percentage of maximum size.
+  private float _lastMaturity = 1.0f; // Used to detect changes.
+
+  private int MACROALGAE_LAYER = 9;
+  private List<GameObject> plants;
+
+  void Start()
+  {
+    this.plants = FindGameObjectsInLayer(this.MACROALGAE_LAYER);
+    this._lastMaturity = this.maturity;
+  }
+
+  void Update()
+  {
+    if (this.maturity != this._lastMaturity) {
+      foreach (GameObject g in this.plants) {
+        g.transform.localScale = new Vector3(this.maturity, this.maturity, this.maturity);
+      }
+    }
+  }
+
+  /**
+   * Finds all objects of a given layer type (referred to by its index).
+   */
+  List<GameObject> FindGameObjectsInLayer(int layer)
+  {
+    GameObject[] allObjects = FindObjectsOfType<GameObject>();
+    List<GameObject> objectsInLayer = new List<GameObject>();
+
+    foreach (GameObject o in allObjects) {
+      if (o.layer == layer) {
+        objectsInLayer.Add(o);
+      }
+    }
+
+    return objectsInLayer;
+  }
+}
