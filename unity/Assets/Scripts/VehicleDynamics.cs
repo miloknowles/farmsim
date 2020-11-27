681dfc78c4dc585e4534f86f961dda47def2a901
Milo Knowles <mknowles@mit.edu>
2020-11-27 18:18:54 -0500

    Lots of dynamics bug fixes

--- unity/Assets/Scripts/VehicleDynamics.cs
+++ unity/Assets/Scripts/VehicleDynamics.cs
@@ -9,12 +9,12 @@ public class VehicleDynamics : MonoBehaviour {
   private float F_rt = 0.0f;  // Force of right thruster.
   private float F_ct = 0.0f;  // Force of center thruster.
 
-  public float C_drag_body = 0.5f * 1027.0f * 1.0f * 0.1f;   // Lump terms: 1/2 * rho * Cd * A
-  public float C_angular_drag_body = 1.0f; // Drag = Cd * w^2
+  public float linearDragCoefficient = 0.5f * 1027.0f * 0.1f * 0.1f;   // Lump terms: 1/2 * rho * Cd * A
+  public float angularDragCoefficient = 0.01f; // Drag = Cd * w^2
 
-  private Vector3 t_lt_body = new Vector3(-0.15f, 0.0f, -0.3f);
-  private Vector3 t_rt_body = new Vector3(0.15f, 0.0f, -0.3f);
-  private Vector3 t_ct_body = new Vector3(0.0f, 0.0f, 0.2f);
+  private Vector3 t_lt_body = new Vector3(-0.1f, 0.0f, -0.2f);
+  private Vector3 t_rt_body = new Vector3(0.1f, 0.0f, -0.2f);
+  private Vector3 t_ct_body = new Vector3(0.0f, 0.0f, 0.05f);
   private Vector3 t_CP_body = new Vector3(0.0f, 0.0f, -0.05f);
 
   void Start()
@@ -30,15 +30,18 @@ public class VehicleDynamics : MonoBehaviour {
 		// Keys: Right = +1, Left = -1
 		float leftRightSign = Input.GetAxis("Horizontal");
 
-    this.F_lt = (leftRightSign < 0) ? 100.0f : 0.0f;
-    this.F_rt = (leftRightSign > 0) ? 100.0f : 0.0f;
-    this.F_ct = -forwardBackwardSign * 100.0f;
+    this.F_lt = (leftRightSign < -0.5f) ? 5.0f : 0.0f;
+    this.F_rt = (leftRightSign > 0.5f) ? 5.0f : 0.0f;
+    this.F_ct = (Mathf.Abs(forwardBackwardSign) > 0.5f) ? -forwardBackwardSign * 10.0f : 0.0f;
   }
 
   void FixedUpdate()
   {
-    Vector3 v_body_world = this.rigidbody.velocity;
-    Vector3 w_body_world = this.rigidbody.angularVelocity;
+    // Get velocity and angular velocity in the body frame.
+    Vector3 v_body = this.transform.InverseTransformDirection(this.rigidbody.velocity);
+    Vector3 w_body = this.transform.InverseTransformDirection(this.rigidbody.angularVelocity);
+
+    Debug.Log(w_body);
 
     // Rear motors create forward (+z thrust).
     Vector3 f_lt = new Vector3(0.0f, 0.0f, this.F_lt);
@@ -48,7 +51,13 @@ public class VehicleDynamics : MonoBehaviour {
     Vector3 f_ct = new Vector3(0.0f, this.F_ct, 0.0f);
 
     // Drag = 1/2 * rho * Cd * A * v^2
-    Vector3 F_drag = -1.0f * this.C_drag_body * Vector3.Scale(v_body_world, v_body_world);
+    Vector3 F_drag = -1.0f * v_body.normalized * this.linearDragCoefficient * Mathf.Pow(v_body.magnitude, 2);
+
+    // NOTE(milo): Need to clamp drag force for stability. Make sure that the force can't causes
+    // an acceration that would change the direction of motion. This prevents drag forces from
+    // wild positive feedback effects.
+    // dv = a * dt = (F / m) * dt < Magnitude(v)
+    F_drag = Vector3.ClampMagnitude(F_drag, v_body.magnitude * this.rigidbody.mass * Time.fixedDeltaTime);
 
     // Compute torques due to motor thrust.
     Vector3 tau_lt = Vector3.Cross(this.t_lt_body, f_lt);
@@ -57,10 +66,10 @@ public class VehicleDynamics : MonoBehaviour {
 
     // Compute torque due to drag.
     Vector3 tau_linear_drag = Vector3.Cross(this.t_CP_body, F_drag);
-    Vector3 tau_angular_drag = this.C_angular_drag_body * Vector3.Scale(w_body_world, w_body_world);
+    Vector3 tau_angular_drag = -1.0f * w_body.normalized * this.angularDragCoefficient * Mathf.Pow(w_body.magnitude, 2);
 
-    // Apply forces and torques to the vehicle.
+    // Apply forces and torques to the vehicle in its body frame.
     this.rigidbody.AddRelativeForce(f_lt + f_rt + f_ct + F_drag);
-    this.rigidbody.AddRelativeTorque(tau_lt + tau_rt + tau_ct + tau_linear_drag + tau_angular_drag);
+    this.rigidbody.AddRelativeTorque(tau_lt + tau_rt + tau_ct + tau_angular_drag);
   }
 }
