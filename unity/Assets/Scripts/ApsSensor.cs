7fbba3bf4b037ecb39a35e8e068a4c350ee14161
Milo Knowles <mknowles@mit.edu>
2021-03-17 15:08:45 -0400

    Aps nosie

--- unity/Assets/Scripts/ApsSensor.cs
+++ unity/Assets/Scripts/ApsSensor.cs
@@ -24,12 +24,19 @@ public class ApsSensor : MonoBehaviour
   public GameObject apsBeaconObject;
   public GameObject apsReceiverObject;
 
+  public bool enableApsNoise = true;
+  public float apsNoiseSigma = 0.1f;
+
   // Lazy read: only get sensor data when called.
   public ApsMeasurement Read()
   {
     long nsec = (long)(Time.fixedTime * 1e9);
     Vector3 t_world_beacon = this.apsBeaconObject.transform.position;
     float range = (this.apsReceiverObject.transform.position - t_world_beacon).magnitude;
+
+    if (this.enableApsNoise && this.apsNoiseSigma > 0) {
+      range += Utils.Gaussian(0, this.apsNoiseSigma);
+    }
 
     // NOTE(milo): Switch to right-handed coordinates!
     return new ApsMeasurement(nsec, range, TransformUtils.ToRightHandedTranslation(t_world_beacon));
