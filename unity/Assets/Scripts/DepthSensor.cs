e4750727d20865d62c5f9f25b1c81f38ac27bb9c
Milo Knowles <mknowles@mit.edu>
2021-03-17 15:01:44 -0400

    Add noise to IMU and depth

--- unity/Assets/Scripts/DepthSensor.cs
+++ unity/Assets/Scripts/DepthSensor.cs
@@ -21,12 +21,21 @@ public class DepthSensor : MonoBehaviour
 {
   public GameObject depthSensorObject;
 
+  public bool enableDepthNoise = true;
+  public float noiseSigma = 0.05f;
+
   public DepthMeasurement Read()
   {
     long nsec = (long)(Time.fixedTime * 1e9);
 
     // NOTE(milo): Unity uses a y-up convention, so flip the sign.
     float depth = -1.0f * this.depthSensorObject.transform.position.y;
+
+    // Optionally add sensor noise.
+    if (this.noiseSigma > 0 && this.enableDepthNoise) {
+      depth += Utils.Gaussian(0, this.noiseSigma);
+    }
+
     return new DepthMeasurement(nsec, depth);
   }
 }
