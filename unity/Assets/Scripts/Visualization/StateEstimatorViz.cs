39e443e5e914f46f7e3f11b027c87c4b218a05bd
Milo Knowles <mknowles@mit.edu>
2021-04-02 16:30:00 -0400

    Finally get MF ghost to show up

--- unity/Assets/Scripts/Visualization/StateEstimatorViz.cs
+++ unity/Assets/Scripts/Visualization/StateEstimatorViz.cs
@@ -21,21 +21,22 @@ public class StateEstimatorViz : MonoBehaviour, LcmSubscriber
   {
     this.lcmHandle = LcmHandle.Singleton;
     this.lcmHandle.Subscribe("vio/filter/world_P_body", this);
+    Debug.Log("Subscribed to VIO channels");
+  }
+
+  void Update()
+  {
+    this.ghostObject.transform.SetPositionAndRotation(this._world_t_body, this._world_q_body);
   }
 
   public void MessageReceived(LcmHandle lcm, string channel, LcmStream dins)
   {
     if (channel == "vio/smoother/world_P_body") {
     } else if (channel == "vio/filter/world_P_body") {
-
       pose3_stamped_t msg = new pose3_stamped_t(dins);
       LCMUtils.unpack_pose3_t(msg.pose, ref this._world_q_body, ref this._world_t_body);
-
       TransformUtils.ToLeftHandedTranslation(this._world_t_body, ref this._world_t_body);
       TransformUtils.ToLeftHandedQuaternion(this._world_q_body, ref this._world_q_body);
-
-      this.ghostObject.transform.SetPositionAndRotation(this._world_t_body, this._world_q_body);
-
     } else {
       Debug.Log("Received pose on unrecognized channel: " + channel);
     }
