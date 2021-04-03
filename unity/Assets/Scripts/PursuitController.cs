fee86e2589db49e22ccbca87a35ab00cac16e6fe
Milo Knowles <mknowles@mit.edu>
2021-04-03 11:52:54 -0400

    More cube following

--- unity/Assets/Scripts/PursuitController.cs
+++ unity/Assets/Scripts/PursuitController.cs
@@ -8,8 +8,8 @@ public class PursuitController : MonoBehaviour
   public Rigidbody rigidBody;
   public GameObject followObject;
   public float pGainPitch = 0.03f;
-  public float pGainYaw = 0.03f;
-  public float pGainThrust = 3.0f;
+  public float pGainYaw = 0.02f;
+  public float pGainThrust = 10.0f;
   private Vector3 unit_forward = new Vector3(0f, 0f, 1f);
   private Vector3 world_t_goal = Vector3.zero;
   private Vector3 body_t_goal = Vector3.zero;
@@ -17,12 +17,11 @@ public class PursuitController : MonoBehaviour
   private Vector3 body_euler_goal = Vector3.zero;
   private Vector3 torque_command = Vector3.zero;
   private Vector3 thrust_command = Vector3.zero;
-
-  // Start is called before the first frame update
-  void Start()
-  {
-
-  }
+  private float turnInPlaceAngle = 20.0f;
+  private float maxForwardVelocity = 3.0f;
+  private float goalPositionTol = 0.2f;
+  private float distVelocityGain = 0.4f;
+  private float maxThrust = 5.0f;
 
   public static float ClampAngle(float deg)
 	{
@@ -30,13 +29,22 @@ public class PursuitController : MonoBehaviour
  	}
 
 
-  public float CalcForwardVelocity(float angle)
+  // Max velocity when pointing directly at target, zero velocity if > turnInPlaceAngle error.
+  public float CalcForwardVelocityFromAngle(float angle)
   {
-    if (angle >= 30.0f) {
+    if (angle >= this.turnInPlaceAngle) {
       return 0.0f;
     }
-    // Goes 2 m/s at 0 angle error.
-    return 2.0f * (30.0f - angle) / 30.0f;
+    return this.maxForwardVelocity * (turnInPlaceAngle - angle) / turnInPlaceAngle;
+  }
+
+  // Set velocity to zero if at target.
+  public float CalcForwardVelocityFromDistance(Vector3 body_t_goal)
+  {
+    float dist = body_t_goal.magnitude;
+    float mag = Mathf.Min(this.maxForwardVelocity, this.distVelocityGain*dist);
+    float sign = body_t_goal.z >= 0.0f ? 1.0f : -1.0f;
+    return sign * mag;
   }
 
   // Update is called once per frame
@@ -49,7 +57,16 @@ public class PursuitController : MonoBehaviour
     this.world_t_goal = this.followObject.transform.position;
 
     this.body_t_goal = this.transform.InverseTransformPoint(this.world_t_goal);
-    this.body_q_goal = TransformUtils.RotateAlignVectors(unit_forward, body_t_goal);
+    this.body_q_goal = TransformUtils.RotateAlignVectors(this.unit_forward, this.body_t_goal);
+
+    float distance_to_goal = this.body_t_goal.magnitude;
+
+    // If at goal, rotate in place to desired orientation.
+    if (distance_to_goal < this.goalPositionTol && this.rigidBody.velocity.magnitude < 0.1f) {
+      // Compute: body_q_goal = body_q_world * world_q_goal;
+      this.body_q_goal = Quaternion.Inverse(this.transform.rotation) * this.followObject.transform.rotation;
+    }
+
     this.body_euler_goal = this.body_q_goal.eulerAngles;
 
     // Apply torques to align with the goal.
@@ -63,10 +80,23 @@ public class PursuitController : MonoBehaviour
 		this.rigidBody.AddRelativeTorque(this.torque_command);
 
     // Forward velocity proportional to angular distance from goal.
-    float desired_vel = CalcForwardVelocity(Quaternion.Angle(this.body_q_goal, Quaternion.identity));
-    float vel_error = desired_vel - this.rigidBody.velocity.magnitude;
+    float desired_vel_ang = CalcForwardVelocityFromAngle(Quaternion.Angle(this.body_q_goal, Quaternion.identity));
 
-    this.thrust_command.z = this.pGainThrust * vel_error;
+    // Forward velocity proportional to metric distance from goal.
+    float desired_vel_dist = CalcForwardVelocityFromDistance(this.body_t_goal);
+    float desired_vel_z = Mathf.Min(desired_vel_ang, desired_vel_dist);
+
+    // Track the desired velocity.
+    float vel_error_z = desired_vel_z - this.transform.InverseTransformDirection(this.rigidBody.velocity).z;
+
+    // Track the desired depth using y-axis thrust.
+    float desired_vel_y = 0.2f * this.body_t_goal.y;
+    float error_vel_y = desired_vel_y - this.transform.InverseTransformDirection(this.rigidBody.velocity).y;
+
+    this.thrust_command.x = 0.0f;
+    this.thrust_command.y = this.pGainThrust * error_vel_y;
+    this.thrust_command.z = this.pGainThrust * vel_error_z;
+    this.thrust_command = Vector3.ClampMagnitude(this.thrust_command, this.maxThrust);
     this.rigidBody.AddRelativeForce(this.thrust_command);
   }
 }
